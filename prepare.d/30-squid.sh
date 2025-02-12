0f50b9c404ac4132d69be3d377e06b6c83920498
Alejandro Visiedo <avisiedo@redhat.com>
2025-02-12 11:40:06 +0100

    feat: prepare proxy
    Signed-off-by: Alejandro Visiedo <avisiedo@redhat.com>

---
+++ prepare.d/30-squid.sh
@@ -0,0 +1,7 @@
+# Install and configure squid http cache
+
+sudo dnf install -y squid
+sudo cp -vf files/etc/squid/* /etc/squid/
+systemctl --quiet is-enabled squid || sudo systemctl enable squid
+systemctl --quiet is-active squid || sudo systemctl start squid
+sudo systemctl reload squid
