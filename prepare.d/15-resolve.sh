0be0321159120534654614b55caddf975fda7f18
Alejandro Visiedo <avisiedo@redhat.com>
2025-02-14 16:45:02 +0100

    fix: dns resolver using DOT
    - Add dns stub.
    - Harden DNS resolution.
    - Flush DNS caches when script is applied.
    
    Signed-off-by: Alejandro Visiedo <avisiedo@redhat.com>

--- prepare.d/15-resolve.sh
+++ prepare.d/15-resolve.sh
@@ -1,6 +1,8 @@
 SERVICE=systemd-resolved
 sudo cp -vf files/etc/systemd/resolved.conf /etc/systemd/resolved.conf
+sudo cp -vf files/etc/resolv.conf /etc/resolv.conf
 systemctl --quiet is-enabled ${SERVICE} || sudo systemctl enable ${SERVICE}
 systemctl --quiet is-active ${SERVICE} || sudo systemctl start ${SERVICE}
 sudo systemctl reload systemd-resolved
+sudo resolvectl flush-caches
 
