4b9eff7abcd2eee54319ad46c4eb6b8b5ac53a86
Alejandro Visiedo <avisiedo@redhat.com>
2025-02-16 15:53:00 +0100

    feat: add Fedora Server ISO image
    Signed-off-by: Alejandro Visiedo <avisiedo@redhat.com>

--- fedora-iso.sh
+++ fedora-iso.sh
@@ -6,6 +6,7 @@ FLAVOR="Fedora-Server"
 FILE_CHECKSUM="${FLAVOR}-41-1.4-aarch64-CHECKSUM"
 FILE_IMAGE="${FLAVOR}-41-1.4.aarch64.raw.xz"
 FILE_QCOW2="${FLAVOR}-KVM-41-1.4.aarch64.qcow2"
+FILE_ISO="${FLAVOR}-dvd-aarch64-41-1.4.iso"
 
 BASE_URL_FEDORA_SERVER="https://ftp-stud.hs-esslingen.de/pub/fedora/linux/releases/41/Server/aarch64/images"
 
@@ -16,6 +17,7 @@ BASE_URL_FEDORA_SERVER="https://ftp-stud.hs-esslingen.de/pub/fedora/linux/releas
 [ -e "${HOME}/Downloads/images/${FILE_CHECKSUM}" ] || curl -o "${HOME}/Downloads/images/${FILE_CHECKSUM}" "${BASE_URL_FEDORA_SERVER}/${FILE_CHECKSUM}"
 [ -e "${HOME}/Downloads/images/${FILE_IMAGE}" ] || curl -o "${HOME}/Downloads/images/${FILE_IMAGE}" "${BASE_URL_FEDORA_SERVER}/${FILE_IMAGE}"
 [ -e "${HOME}/Downloads/images/${FILE_QCOW2}" ] || curl -o "${HOME}/Downloads/images/${FILE_QCOW2}" "${BASE_URL_FEDORA_SERVER}/${FILE_QCOW2}"
+[ -e "${HOME}/Downloads/images/${FILE_ISO}" ] || curl -o "${HOME}/Downloads/images/${FILE_ISO}" "${BASE_URL_FEDORA_SERVER}/${FILE_ISO}"
 
 # Check CHECKSUM
 gpgv --keyring "$HOME/Downloads/images/fedora.gpg" "$HOME/Downloads/images/${FILE_CHECKSUM}"
