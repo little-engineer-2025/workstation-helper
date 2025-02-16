b18c7c23ae0ddac439e40c4262f7b85306a8ec9a
Alejandro Visiedo <avisiedo@redhat.com>
2025-02-16 15:56:56 +0100

    chore: add manual steps
    - Add manual steps to update the repositories, so dnf uses
      https protocol, and through the proxy server.
    
    Signed-off-by: Alejandro Visiedo <avisiedo@redhat.com>

--- prepare.sh
+++ prepare.sh
@@ -6,3 +6,9 @@ for item in ./prepare.d/*.sh; do
   echo -e "\n>> Processing '${item}'"
   source "${item}"
 done
+
+cat <<EOF
+Manual tasks:
+- Add `proxy=http://127.0.0.1:3128` at /etc/yum.repos.d/*.repo files.
+- Add `protocol=https` at /etc/yum.repos.d/*.repo files.
+EOF
