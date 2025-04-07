d23863554e49cb2507f2aa7b0132f8a2667730bf
Alejandro Visiedo <alejandro.visiedo@gmail.com>
2025-04-07 05:56:43 +0200

    fix: override connectivify-fedora configuration
    This is necessary to avoid the file could be overwritten
    by some upgrade or reinstallation. This warranty the values
    will be overriden.
    
    Be aware this apply only networkmanager connectivity; browsers
    provides their own mechanism to check connectivity and
    show up the captive portal when is present.
    
    Signed-off-by: Alejandro Visiedo <alejandro.visiedo@gmail.com>

--- prepare.d/10-nmcli.sh
+++ prepare.d/10-nmcli.sh
@@ -5,3 +5,4 @@ CONN_NAME="e760b99c-999c-3143-9dbc-f321ea211f09"
 nmcli con modify "${CONN_NAME}" connection.autoconnect 1
 nmcli con modify "${CONN_NAME}" ipv4.dns 127.0.0.53
 nmcli con modify "${CONN_NAME}" ipv4.ignore-auto-dns 1
+cp -vf files/usr/lib/NetworkManager/conf.d/90-connectivity-fedora.conf /usr/lib/NetworkManager/conf.d/90-connectivity-fedora.conf
