38bfa7d46affe5cb6b4df09f3d3e32cbcfefc0e2
Alejandro Visiedo <alejandro.visiedo@gmail.com>
2025-05-07 13:21:35 +0200

    fix: firewall rules
    - Add additional DNS servers.
    - Update default INPUT policy to drop.
    - Reorder ICMP rules.
    
    Signed-off-by: Alejandro Visiedo <alejandro.visiedo@gmail.com>

--- prepare.d/20-firewall.sh
+++ prepare.d/20-firewall.sh
@@ -47,12 +47,15 @@ initial_arp_filter() {
 
 setup_firewall() {
   sudo mkdir -p /etc/nftables/custom || true
-  sudo cp -vf files/etc/nftables/custom/* /etc/nftables/custom/
+  sudo chmod 0600 /etc/nftables/custom/{arp,drop-all,main,nat,router}.nft
+  sudo cp -vf files/etc/nftables/custom/{arp,drop-all,main,nat,router}.nft /etc/nftables/custom/
+  sudo chmod 0400 /etc/nftables/custom/{arp,drop-all,main,nat,router}.nft
   sudo cp -vf files/usr/lib/systemd/system/nft-custom.service /usr/lib/systemd/system/nft-custom.service
   ! systemctl is-active firewalld &>/dev/null || sudo systemctl stop firewalld
   ! systemctl is-enabled firewalld &>/dev/null || sudo sh -c "systemctl disable firewalld; systemctl mask firewalld"
   systemctl is-enabled nft-custom &>/dev/null || sudo sh -c "systemctl enable nft-custom; systemctl unmask nft-custom"
   systemctl is-active nft-custom &>/dev/null || sudo systemctl start nft-custom
+  sudo systemctl daemon-reload &>/dev/null
   sudo systemctl reload nft-custom &>/dev/null
   sudo cp -vf ./files/usr/local/bin/arp-add /usr/local/bin/
   sudo cp -vf ./files/usr/local/bin/arp-delete /usr/local/bin/
